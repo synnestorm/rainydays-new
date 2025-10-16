d1c4a3b34cbf731215a51ac8e6e975101d6b2347
synnestorm <synne.storm@gmail.com>
2025-10-16 21:25:01 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic3lubmVzdG9ybSIsImVtYWlsIjoic3luc3RvMDQxMTZAc3R1ZC5ub3JvZmYubm8iLCJpYXQiOjE3NjA2NDIyODZ9.KOqvvVx5dORKTu_CjmYZ5xMZEzJ4JGy1H5kFxssSANY";
 const options = {
   headers: {
-    Authorization: `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic3lubmVzdG9ybSIsImVtYWlsIjoic3luc3RvMDQxMTZAc3R1ZC5ub3JvZmYubm8iLCJpYXQiOjE3NjA2NDIyODZ9.KOqvvVx5dORKTu_CjmYZ5xMZEzJ4JGy1H5kFxssSANY}`, // my short-lived token; this will need to be updated
+    Authorization: `Bearer ${token}`, // my short-lived token; this will need to be updated
     "3a404182-2ca7-44dd-8445-61b7b878840f": apiKey.data.key, // my long-lived API key
   },
 };
