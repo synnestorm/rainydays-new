94655c07ad6fa9f92bb027097eeee46c334e7a31
synnestorm <synne.storm@gmail.com>
2025-10-16 21:11:21 +0200

    update comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const options = {
   headers: {
-    Authorization: `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic3lubmVzdG9ybSIsImVtYWlsIjoic3luc3RvMDQxMTZAc3R1ZC5ub3JvZmYubm8iLCJpYXQiOjE3NjA2Mzk4MzV9.ZA4kVTLgWITqte46sk01bHxwGLrzsA8zkP5TlHzULUU}`, // your short-lived token
-    "ddc32717-7e81-4b7d-8d17-b297cf026e79": apiKey.data.key, // your long-lived API key
+    Authorization: `Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic3lubmVzdG9ybSIsImVtYWlsIjoic3luc3RvMDQxMTZAc3R1ZC5ub3JvZmYubm8iLCJpYXQiOjE3NjA2Mzk4MzV9.ZA4kVTLgWITqte46sk01bHxwGLrzsA8zkP5TlHzULUU}`, // my short-lived token; this will need to be updated
+    "ddc32717-7e81-4b7d-8d17-b297cf026e79": apiKey.data.key, // my long-lived API key
   },
 };
 
