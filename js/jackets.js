e8494679ebf208757e585c28c1bfb18f625f0364
synnestorm <synne.storm@gmail.com>
2025-10-24 21:13:51 +0100

    testing dynamic js

--- js/jackets.js
+++ js/jackets.js
@@ -1,5 +1,6 @@
 // for the jackets pages
 
+const jacketsList = document.querySelector("#jackets-list")
 const apiUrl = "https://v2.api.noroff.dev/rainy-days"
 
 async function fetchProducts() {
@@ -7,7 +8,17 @@ async function fetchProducts() {
         const response = await fetch(apiUrl)
         const products = response.json()
 
-        console.log(products);
+        products.forEach(product => {
+            const display = document.createElement("div")
+            const image = document.createElement("img")
+            const title = document.createElement("p")
+            const price = document.createElement("span")
+
+            display.className = "display"
+            image.className = "jacket-image"
+            title.className = "about-product"
+            price.className = "price"
+        });
     } catch (error) {
         console.log("Could not fetch products, try again later", error)
     }
