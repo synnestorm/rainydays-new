e4e7835a4a05122b214d58a97905c2a11a33510d
synnestorm <synne.storm@gmail.com>
2025-10-24 20:15:04 +0100

    fetch API data

--- js/jackets.js
+++ js/jackets.js
@@ -1,1 +1,14 @@
 // for the jackets pages
+
+const apiUrl = "https://v2.api.noroff.dev/rainy-days"
+
+async function fetchProducts() {
+    try {
+        const response = await fetch(apiUrl)
+        const products = response.json()
+
+        console.log(products);
+    } catch (error) {
+        console.log("Could not fetch products, try again later", error)
+    }
+}
