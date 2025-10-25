a8573347e34cf9538a74a2163c4e1ed730139ad9
synnestorm <synne.storm@gmail.com>
2025-10-25 20:45:02 +0100

    adding const var

--- js/product-details.js
+++ js/product-details.js
@@ -1,1 +1,4 @@
-// for the induvidual jacket pages; displaying details about jackets
+// for the induvidual jacket pages; displaying details about different jackets
+
+const jacketDetail = document.querySelector("#jacket-detail")
+const apiUrl = "https://v2.api.noroff.dev/rainy-days"
