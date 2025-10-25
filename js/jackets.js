5a9d56f2f8078717d76c6aab306f08aa4d90e125
synnestorm <synne.storm@gmail.com>
2025-10-25 13:20:51 +0100

    update js

--- js/jackets.js
+++ js/jackets.js
@@ -14,27 +14,30 @@ async function fetchProducts() {
             card.className = "card"
 
             const link = document.createElement("a")
-            link.href = `index.html`
+            link.href = `product-details.html?id=${product.id}`
             link.className = "product-link"
 
             const image = document.createElement("img")
-            const title = document.createElement("p")
-            const price = document.createElement("span")
-
-            image.className = "jacket-image"
-            title.className = "about-product"
-            price.className = "price"
-
             image.src = product.image.url
             image.alt = product.image.alt
-            title.textContent = product.title
-            price.textContent = product.price
+            image.className = "jacket-image"
 
+            const title = document.createElement("p")
+            title.textContent = product.title
+            title.className = "about-product"
+
+            const price = document.createElement("span")
+            price.className = "price"
+            price.textContent = `${product.price} NOK`
+
+            card.appendChild(image)
             card.appendChild(title)
             card.appendChild(price)
-            card.appendChild(image)
 
-            jacketsList.appendChild(card)
+            link.appendChild(card)
+            
+
+            jacketsList.appendChild(link)
         })
     } catch (error) {
         console.error("Failed to fetch products", error)
