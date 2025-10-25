9e78fa057d8200ec68b02ce2d7e27e8796ad2e7f
synnestorm <synne.storm@gmail.com>
2025-10-25 21:02:03 +0100

    update function

--- js/product-details.js
+++ js/product-details.js
@@ -7,6 +7,14 @@ async function fetchProduct() {
     try {
         const params = new URLSearchParams(window.location.search)
         const id = params.get("id")
+
+        if (!id) {
+            jacketDetail.textContent = "No product ID provided!"
+            return
+        }
+        const response = await fetch(`${apiUrl}/${id}`)
+        const data = await response.json()
+        const product = data.data
     } catch (error){
 
     }
