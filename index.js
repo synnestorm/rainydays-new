f26ed9842102c412791cf7233e049e9eac51a027
synnestorm <synne.storm@gmail.com>
2025-10-26 21:48:42 +0000

    variables

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const apiUrl = "https://v2.api.noroff.dev/rainy-days"
 
 async function fetchNewArrivals() {
     try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        const products = data.data
+
+        const arrivalDiv = document.createElement("div")
+        arrivalDiv.className = "product-arrivals"
+
+        const image = document.createElement("image")
+        image.className = "product-image"
+
+        const price = document.createElement("span")
+        price.className = "product-price"
 
     } catch (error) {
         console.error("Failed to fetch new arrivals", error)
