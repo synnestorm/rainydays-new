1361a6505de2b4b9fe4d59d00856b5695b61decb
synnestorm <synne.storm@gmail.com>
2025-10-26 19:06:43 +0000

    create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// for the index page
+
+const newArrivals = document.querySelector("#new-arrivals")
+const apiUrl = "https://v2.api.noroff.dev/rainy-days"
+
+async function fetchNewArrivals() {
+    try {
+
+    } catch (error) {
+        console.error("Failed to fetch new arrivals", error)
+    }
+}
