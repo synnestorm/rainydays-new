404eaca891be2ba45342a4c37cd4de066e15cb94
synnestorm <synne.storm@gmail.com>
2025-10-26 22:21:30 +0000

    js for index.html

--- index.js
+++ index.js
@@ -6,19 +6,37 @@ const apiUrl = "https://v2.api.noroff.dev/rainy-days"
 async function fetchNewArrivals() {
     try {
         const response = await fetch(apiUrl)
+        if (!response.ok) {
+            throw new Error(`Error! Status: ${response.status}`)
+        }
+        
         const data = await response.json()
         const products = data.data
 
+        const selectedArrivals = products.slice(3, 6)
+
+        newArrivals.innerHTML = "";
+
+        selectedArrivals.forEach(product => {
         const arrivalDiv = document.createElement("div")
         arrivalDiv.className = "product-arrivals"
 
-        const image = document.createElement("image")
+        const image = document.createElement("img")
         image.className = "product-image"
+        image.src = product.image.url
+        image.alt = product.title
 
         const price = document.createElement("span")
         price.className = "product-price"
+        price.textContent = `${product.price} NOK`
 
+        arrivalDiv.appendChild(image)
+        arrivalDiv.appendChild(price)
+        newArrivals.appendChild(arrivalDiv)
+})
     } catch (error) {
         console.error("Failed to fetch new arrivals", error)
     }
 }
+
+fetchNewArrivals()
