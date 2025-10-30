b6b887719075c8e154916526795bf2da5fabf27f
synnestorm <synne.storm@gmail.com>
2025-10-30 17:21:19 +0000

    generate cart function

--- checkout/cart.js
+++ checkout/cart.js
@@ -16,6 +16,35 @@ function generateCart() {
     cart.forEach((item, index) => {
         total += item.price * item.quantity
 
-        
+        const productDisplay = document.createElement("div")
+        productDisplay.className = "product-display"
+
+        const image = document.createElemenet("img")
+        image.className = "product-image"
+        image.src = item.image
+        image.alt = item.title
+
+        const title = document.createElement("h2")
+        title.className = "product-title"
+
+        const price = document.createElement("span")
+        price.className = "product-price"
+        price.textContent = `${item.price} NOK`
+
+        const removeBtn = document.createElement("button")
+        removeBtn.className = "remove-btn"
+        removeBtn.textContent = "Remove products"
+        removeBtn.addEventListener ("click", () => removeFromCart(index))
+
+        productDisplay.appendChild(img)
+        productDisplay.appendChild(title)
+        productDisplay.appendChild(price)
+        productDisplay.appendChild(removeBtn)
+
+        cartContainer.appendChild(productDisplay)
+
+
     })
+
+    cartProducts.textContent = `All products: ${total} NOK`
 }
